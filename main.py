161920c28c809263c121b34a76ebb847328efbba
ThomasMcCauley <tmccaul4@uncc.edu>
2024-07-09 20:19:46 -0400

    feat: fastAPI setup with endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from typing import Union
 from fastapi import FastAPI
-from typing import Optional
 
 app = FastAPI()
 
@@ -8,5 +8,5 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
